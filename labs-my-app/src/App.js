b8f9a394c5ca4953d32359c07842e5c93a248b72
Acvamarin <makavchik.kirill@mail.ru>
2019-10-23 19:56:53 +0300

    auth with form updates

--- labs-my-app/src/App.js
+++ labs-my-app/src/App.js
@@ -1,18 +1,56 @@
-import Form from "./components/FormAddUsers/FormAddUsers";
 import React, { Component } from "react";
+import Login from "./components/login";
+import Form from "./components/Form";
+import Home from "./components/home";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { isAuthenticated } from "./respository";
 
 class App extends Component {
-
+  logOut() {
+    localStorage.removeItem("x-access-token");
+  }
 
   render() {
-   
-
     return (
-      <div >
-        <Form/>
-      </div>
+      <Router>
+        <div>
+          <nav className="navbar navbar-default">
+            <div className="container-fluid container">
+              <div className="navbar-header"></div>
+              <ul className="nav navbar-nav">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+
+                <li>
+                  {isAuthenticated() ? (
+                    <Link to="/tip/special">Special Tips</Link>
+                  ) : (
+                    ""
+                  )}
+                </li>
+              </ul>
+              <ul className="nav navbar-nav navbar-right">
+                {isAuthenticated() ? (
+                  <li onClick={this.logOut}>
+                    <a href="/">Log out</a>{" "}
+                  </li>
+                ) : (
+                  <li>
+                    <Link to="/login">Log in</Link>
+                  </li>
+                )}
+              </ul>
+            </div>
+          </nav>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/tip/special" component={Form} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Router>
     );
   }
 }
 
 export default App;
+
---
+++ labs-my-app/src/components/Form.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import FormAddUsers from "./FormAddUsers/FormAddUsers";
+
+class Form extends Component {
+  render() {
+    return (
+      <div>
+        <FormAddUsers/>
+      </div>
+    );
+  }
+}
+
+export default Form;
